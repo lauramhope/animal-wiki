30445b937c46eb3436f72ff16b13bd6ae1254207
Laura Hope <ssteamlaura@gmail.com>
2023-03-22 13:31:40 -0700

    Add template to README.md.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 window.onload = function() {
-  const form = document.querySelector("form");
-  form.onsubmit = function(event) {
-    event.preventDefault();
+  const form = document.querySelector("form"); // the <form> tag
+  form.onsubmit = function(event) { // the event handler is onsubmit, creates a function to happen on submission
+    event.preventDefault(); // prevents the page from defaulting to refresh/reload upon submission
 
-    let turtleFacts = document.getElementById("turtles");
-    turtleFacts.setAttribute("class", "hidden");
+    let turtleFacts = document.getElementById("turtles"); // id is from div section for turtles
+    turtleFacts.setAttribute("class", "hidden"); // variable set to that entire div section, hides it (CSS display for the id .hidden)
     let penguinFacts = document.getElementById("penguins");
     penguinFacts.setAttribute("class", "hidden");
     let elephantFacts = document.getElementById("elephants");
@@ -12,16 +12,16 @@ window.onload = function() {
     let notListed = document.getElementById("notListed")
     notListed.setAttribute("class", "hidden");
 
-    const animal = document.querySelector("input#animal").value;
+    const animal = document.querySelector("input#animal").value; // set value to whatever is written in the submission box (input, id is animal)
 
-    if (animal === "turtles") {  
-      turtleFacts.removeAttribute("class");
-    } else if (animal === "penguins") { 
-      penguinFacts.removeAttribute("class");
-    } else if (animal === "elephants") {
-      elephantFacts.removeAttribute("class");
-    } else {
-      notListed.removeAttribute("class");
+    if (animal === "turtles") {  // if the input is "turtles"
+      turtleFacts.removeAttribute("class"); // the hidden class will be removed, showing turtle facts
+    } else if (animal === "penguins") {  // if the input is "penguins"
+      penguinFacts.removeAttribute("class"); // the hidden class will be removed, showing penguin facts
+    } else if (animal === "elephants") { // if the input is elephants
+      elephantFacts.removeAttribute("class"); // the hidden class will be removed, showing elephant facts
+    } else { // if ANYTHING else is written in the input box...
+      notListed.removeAttribute("class"); // will show the "notListed" variable
     }
   };
 };
